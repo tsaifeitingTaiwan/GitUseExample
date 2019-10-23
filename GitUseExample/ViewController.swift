7c3ecae2269092a61b9260bfacbd41d89c8c74a1
Silvia Tsai <silviatsaipatta@outlook.com>
2019-10-23 11:53:27 +0800

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Silvia Tsai on 2019/10/23.
+//  Copyright © 2019 PattatTek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
